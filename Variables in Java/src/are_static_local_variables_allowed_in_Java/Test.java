045bcfb74295d3976a27800c5d791df46c04d9b0
deshanr <deshanr@QAPPS02.QAPPSTECH.IN>
2019-09-10 18:15:23 +0530

    commit

--- Variables in Java/src/are_static_local_variables_allowed_in_Java/Test.java
+++ Variables in Java/src/are_static_local_variables_allowed_in_Java/Test.java
@@ -6,10 +6,10 @@ package are_static_local_variables_allowed_in_Java;
 public class Test 
 {
 
-	static int demo()
+//	static int demo()
 	{
-	    static int x = 10; // //Error: Static local variables are not allowed 
-		return x--;
+//	    static int x = 10; // //Error: Static local variables are not allowed 
+//		return x--;
 		
 	}
 	
